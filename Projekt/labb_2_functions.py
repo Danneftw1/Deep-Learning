4e38ec91580394f3a9b95961a36cccd5dac0ddd8
Daniel Nilsson <daniel.nilsson1@iths.se>
2024-01-11 11:47:46 +0100

    continued with project abit

--- Projekt/labb_2_functions.py
+++ Projekt/labb_2_functions.py
@@ -1,5 +1,8 @@
 from pandas import DataFrame, Series
 from statistics import mean, median
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 def character_counter_for_sentences(column: Series) -> None:
     sentence_length_characters = []
@@ -31,3 +34,22 @@ def print_metrics_for_characters_or_words(word_or_character: str, word_or_charac
     print_min = print(f"Smallest Length {word_or_character}:", min(word_or_character_list))
     print_max = print(f"Longest Length {word_or_character}:", max(word_or_character_list),"\n")
     return print_median, print_mean, print_min, print_max
+
+
+def length_words_and_characters_histplot(df,
+    size_x=1, size_y=2, figsizes=(15, 5), bin=50, kde=True
+) -> sns.histplot: 
+    fig, axs = plt.subplots(size_x, size_y, figsize=figsizes)
+
+    sns.histplot(df["character_amount"], bins=bin, kde=kde, ax=axs[0])
+    axs[0].set_title("Sentence length in characters")
+    axs[0].set_xlabel("Characters")
+    axs[0].set_ylabel("Amount")
+
+    sns.histplot(df["word_amount"], bins=bin, kde=kde, ax=axs[1])
+    axs[1].set_title("Sentence length in words")
+    axs[1].set_xlabel("Words")
+    axs[1].set_ylabel("Amount")
+
+    plt.tight_layout()
+    plt.show()
